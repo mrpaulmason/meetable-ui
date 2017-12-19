ef36d869a8a931e45207c284c7240187c9dcdda4
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-12-19 15:15:30 -0500

    composing with Redux dev tools and applying thunk middleware

---
+++ src/components/Invite.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Invite extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Invite
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import meetableApp from './reducers/';
 import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(meetableApp);
-
-let store = createStore(meetableApp)
+let store = createStore(meetableApp, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
