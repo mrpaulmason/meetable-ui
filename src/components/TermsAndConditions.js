0ab2cadf07e160648cb58efa8e707b98d291aa33
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-12-21 12:38:31 -0500

    updated terms and conditions component with dummy text

--- src/components/TermsAndConditions.js
+++ src/components/TermsAndConditions.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const TermsAndConditions = () => {
   return (
-    <div>
-      <h1>Terms and Conditions</h1>
+    <div className='terms-and-conditions'>
+      <div>
+      <h2>Terms and Conditions</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet sem mauris, at pulvinar nisi ultrices  eu. Proin vel libero diam. Sed vitae odio tempor, pretium dui ut, faucibus leo. Nunc efficitur justo nunc. Aenean et lacus risus. Duis venenatis orci ut justo euismod, quis rhoncus libero eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porta accumsan hendrerit. Nunc mollis ligula eu metus luctus consequat. Ut      sodales risus sed urna dapibus, dictum pharetra ex bibendum. Aliquam aliquam mi at enim pellentesque tristique. Vivamus pulvinar eu tellus in volutpat.
+
+        Donec luctus risus at quam vestibulum vehicula. Duis sed leo neque. Praesent mollis ultrices mi eget venenatis. Aenean placerat luctus nulla, non fringilla neque accumsan vitae. Nunc varius urna ut quam porttitor tempor. Nulla vel purus non nibh ultrices tincidunt. Proin efficitur facilisis fermentum. Nulla vehicula rhoncus augue, sit amet tempor metus auctor vel.
+
+        Etiam tincidunt sit amet augue id porttitor. Proin sed feugiat leo. Nullam sit amet tortor auctor, venenatis dolor ac, condimentum leo. Donec vestibulum neque non nunc auctor elementum. Nam posuere vulputate tellus, quis mattis enim facilisis nec. Etiam ac lacus nunc. Suspendisse ultrices blandit interdum. Aliquam tincidunt non arcu a tincidunt. Integer ut libero et libero ultricies imperdiet. In efficitur tellus ac ex fringilla aliquet eu tincidunt arcu. Pellentesque vestibulum magna ac odio volutpat, sed accumsan orci semper. Donec vitae lacus at urna ornare sodales. Etiam sodales purus id ornare tempor.
+
+        Cras eget ornare ligula. Integer facilisis facilisis lorem. Aenean ac lacinia leo. Aliquam turpis magna, suscipit quis pharetra sit amet, facilisis id arcu. Nulla auctor libero in orci porttitor dignissim. Sed dapibus condimentum ante quis commodo. Maecenas mattis, diam fermentum lacinia rhoncus, lorem nisl porta mi, sed vulputate turpis felis ac neque. Aliquam tempor at metus vel varius. Nullam hendrerit rhoncus turpis, quis mattis purus dapibus quis.
+
+        Pellentesque pretium sem at tortor congue, ac maximus erat porttitor. Phasellus consequat feugiat aliquet. Aliquam venenatis augue ac massa euismod, nec dapibus quam malesuada. Phasellus faucibus vehicula ante, ac luctus tortor porta et. Curabitur euismod, justo ac suscipit fermentum, mi odio interdum eros, eget pretium nunc neque sed sem. Phasellus mollis orci erat, a tincidunt ligula lobortis vel. Nunc sed dui sollicitudin, pretium mauris ac, posuere tellus. Cras sed justo nunc. Curabitur rhoncus efficitur ipsum, id consequat est consequat non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac ultrices quam. In laoreet lectus sit amet ipsum aliquam eleifend. Curabitur bibendum orci a purus commodo, vitae auctor sapien convallis. Fusce tempor velit eu elit posuere, id ullamcorper arcu mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+        Donec eget nunc non odio maximus pretium. Aliquam sodales dignissim mi, vel scelerisque neque congue quis. Proin purus magna, iaculis vitae ultrices id, placerat vitae metus. In est dolor, volutpat eu justo quis, sagittis pretium arcu. In placerat enim erat. Nullam sollicitudin lorem et commodo condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam in nunc lectus. Proin dictum velit vel dapibus mattis. Maecenas dolor velit, elementum eu orci eu, viverra dignissim ipsum. Morbi elit ligula, efficitur eu velit id, finibus fringilla ex. Aliquam egestas lobortis nibh a dictum. Pellentesque laoreet semper nibh non scelerisque.
+
+        Cras venenatis nisi purus. Praesent consectetur quis lacus eu feugiat. Quisque id diam vitae risus venenatis iaculis. Praesent cursus tincidunt orci, sit amet venenatis velit vestibulum ut. Mauris a placerat nibh. Nullam accumsan, ligula at ultricies gravida, ante est placerat nisi, sed lobortis diam lectus in quam. Mauris in tempor elit. Suspendisse nec urna vestibulum ligula auctor imperdiet. Quisque consectetur odio vel fringilla fermentum. Cras ut finibus magna. Aliquam in mattis justo. Maecenas lobortis mauris vel purus ornare, vitae consectetur turpis dignissim. Aliquam sed turpis dictum, vestibulum nisl nec, bibendum nisi.
+
+        Nunc magna sem, efficitur imperdiet faucibus vitae, rutrum eleifend tortor. Fusce nibh ante, congue non posuere quis, eleifend ut lorem. Sed cursus elit nec nunc blandit porttitor. Sed commodo sollicitudin massa ac pharetra. Ut sed eros massa. Donec eu magna vel nisl pellentesque vulputate nec et nulla. Vivamus finibus nunc eget dictum tempor. Nulla facilisi.
+
+        Sed nec placerat nisl. Nam eget erat nibh. Aliquam gravida lacinia nisi ac porttitor. Cras elementum id augue id fermentum. Cras ac ligula egestas, lacinia nulla at, faucibus elit. Quisque rutrum mauris eu ex aliquam sodales. Vestibulum sodales ullamcorper consequat. In facilisis lectus mi, in tempor ante lacinia vel. Integer vitae dolor turpis. In hac habitasse platea dictumst. Aliquam in sagittis urna. Integer dictum enim nunc, cursus ullamcorper elit dapibus quis. Morbi tristique euismod fermentum. Curabitur eros mi, eleifend iaculis pharetra viverra, fermentum non augue. In hac habitasse platea dictumst.
+
+        Morbi ac erat auctor, tincidunt lacus sed, vulputate dolor. Maecenas dictum, quam id rutrum congue, nunc tortor dapibus tellus, ac vulputate ipsum odio at mauris. Morbi facilisis eros eros, quis venenatis ante ultricies nec. Praesent ultricies in nunc eu elementum. Nam tempus imperdiet velit ac cursus. Sed fermentum diam risus, quis egestas magna sagittis eu. Ut porta eros mauris, eu tincidunt libero iaculis in. In turpis nunc, rutrum ac rhoncus vel, sollicitudin tincidunt nisl. Mauris vel lacinia eros, vitae venenatis risus. Duis vestibulum non ex id consequat. Mauris nec arcu iaculis, malesuada turpis vel, ullamcorper nulla. Donec lectus eros, pharetra a semper sed, ultrices eget nisl.
+      </p>
+      </div>
     </div>
   )
 }
