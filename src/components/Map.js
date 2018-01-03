bbb2d621eb075e8f4c665ccdc3d41601c9aa617f
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-01-03 14:39:13 -0500

    replaced mapbox map with google map embed

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoianR5bmVyYnJ5YW4iLCJhIjoiY2piaTQ1cWdhMXR1MDM0cXd0OHNnbWV6eiJ9.dRUmFyjWhDnI_EnqUxNMfw'
+mapboxgl.accessToken = 'pk.eyJ1IjoianR5bmVyYnJ5YW4iLCJhIjoiY2piaTQ1cWdhMXR1MDM0cXd0OHNnbWV6eiJ9.dRUmFyjWhDnI_EnqUxNMfw';
 
 class Map extends React.Component {
-
-  componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [-73.9923, 40.7350],
-      zoom: 12.00
-    });
-  }
-
-  componentWillUnmount() {
-    this.map.remove();
-  }
+  // componentDidMount() {
+  //   this.map = new mapboxgl.Map({
+  //     container: this.mapContainer,
+  //     style: 'mapbox://styles/mapbox/streets-v9',
+  //     center: [-73.9923, 40.735],
+  //     zoom: 12.0
+  //   });
+  // }
+  //
+  // componentWillUnmount() {
+  //   this.map.remove();
+  // }
 
   render() {
-    return <div className='map' ref={el => this.mapContainer = el} />;
+    return (
+      <div>
+        <iframe title="google map" src="https://www.google.com/maps/d/embed?mid=1LFdD2Ort1ausi0gK7tSUPTFQOOHdd-DP" width="800" height="650" />
+      </div>
+    );
   }
 }
 
-export default Map
+export default Map;
