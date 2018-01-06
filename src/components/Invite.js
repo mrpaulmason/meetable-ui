fdbe60369f8b73549d7f54bc7855f5c1ed4c6441
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-01-06 13:08:26 -0500

    recreating Invite component to match new designs

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -1,91 +1,104 @@
 import React from 'react';
-import Navbar from './Navbar'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { postRefAndPhoneNumber, setRef, setPhoneNumber } from '../actions/index'
+import { postRefAndPhoneNumber, setRef, setPhoneNumber } from '../actions/index';
 
 class Invite extends React.Component {
-
   state = {
     phoneNumber: '',
     ref: this.props.location.pathname.slice(1),
     termsAccepted: false
-  }
+  };
 
-  validatePhoneNumber = (input) => {
+  validatePhoneNumber = input => {
     let phoneNumber = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    return input.match(phoneNumber) ? true : false
-  }
+    return input.match(phoneNumber) ? true : false;
+  };
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
       phoneNumber: e.target.value
-    })
-  }
+    });
+  };
 
   handleCheckbox = () => {
     if (this.state.termsAccepted === false) {
       this.setState({
         termsAccepted: true
-      })
+      });
     } else {
       this.setState({
         termsAccepted: false
-      })
+      });
     }
-  }
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
+  handleSubmit = e => {
+    e.preventDefault();
 
     if (!this.validatePhoneNumber(this.state.phoneNumber)) {
-      alert('Please enter a valid phone number')
+      alert('Please enter a valid phone number');
     } else if (!this.state.termsAccepted) {
-      alert('You must accept the Terms and Conditions')
+      alert('You must accept the Terms and Conditions');
     } else {
-      this.props.setRef(this.state.ref)
-      this.props.setPhoneNumber(this.state.phoneNumber)
-      this.props.postRefAndPhoneNumber(this.state.ref, this.state.phoneNumber)
+      this.props.setRef(this.state.ref);
+      this.props.setPhoneNumber(this.state.phoneNumber);
+      this.props.postRefAndPhoneNumber(this.state.ref, this.state.phoneNumber);
       this.setState({
         phoneNumber: ''
-      })
+      });
     }
-  }
+  };
 
   render() {
-    if (this.props.user.postResults !== '' && this.props.user.postResults !== 'invalid code') {
-      alert(this.props.user.postResults)
-    }
+    let style = {
+      backgroundColor: '#663399'
+    };
+
+    //
+    // if (this.props.user.postResults !== '' && this.props.user.postResults !== 'invalid code') {
+    //   alert(this.props.user.postResults);
+    // }
     return (
-      <div className='invite'>
-        <Navbar/>
-        <form onSubmit={this.handleSubmit}>
-          <h3>Phone Number</h3>
-          <input type='text' value={this.state.phoneNumber} onChange={this.handleChange}></input>
-          <input type='checkbox' onClick={this.handleCheckbox}></input>
-          <h3 id='terms-header'>I have read and agree to the<a href='/privacy' id='terms-link' target="_blank">Terms and Conditions</a></h3>
-          <br/>
-          <input type='submit' value='Get Started'></input>
+      <div className="invite">
+        <h1 id="logo-box">M</h1>
+        <h1>Meetable</h1>
+        <p>AI for meeting up with work peeps</p>
+        <form>
+          <input type="text" placeholder="Default text (e.g., Enter number)" onChange={this.handleChange} />
+          <div className="inline-field">
+            <input type="checkbox" onClick={this.handleCheckbox} />
+            <label>
+              I accept the{' '}
+              <a href="/privacy" id="terms-link" target="_blank">
+                terms of service
+              </a>
+            </label>
+          </div>
         </form>
-        <div className='footer'>
+        <div>
+          <p className="copyright">2018 Meetable</p>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStatetoProps = (state) => {
+const mapStatetoProps = state => {
   return {
     user: state.user
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    postRefAndPhoneNumber,
-    setRef,
-    setPhoneNumber
-  }, dispatch)
-}
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      postRefAndPhoneNumber,
+      setRef,
+      setPhoneNumber
+    },
+    dispatch
+  );
+};
 
-export default connect(mapStatetoProps, mapDispatchToProps)(Invite)
+export default connect(mapStatetoProps, mapDispatchToProps)(Invite);
