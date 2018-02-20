1761e54b71c27da817d70528c55961f5837ed8b2
David Gavazzi <dgavazzi@users.noreply.github.com>
2018-02-20 12:21:58 -0500

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -68,7 +68,7 @@ class LandingPage extends React.Component {
       <div className="landing-page">
         <img src={logo} alt='logo' style={{width: '100px', marginLeft: '-10px', marginTop: '30px'}}/>
         <h1>
-         <img src={wordmark} alt='Meetable' style={{width: '225px'}}/>
+         <img src={wordmark} alt='Meetable' style={{width: '275px'}}/>
         </h1>
         <div>
           <p>AI</p>
