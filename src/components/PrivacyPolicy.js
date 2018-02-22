ca94e4dd11e3837a4d2a62ea82cff2e5ff557d20
David Gavazzi <dgavazzi@users.noreply.github.com>
2018-02-22 13:14:26 -0500

    Update PrivacyPolicy.js

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -3,6 +3,7 @@ import React from 'react';
 const PrivacyPolicy = () => {
   return (
     <div className='landing-page'>
+    <div>
         <h2>Privacy Policy</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet sem mauris, at pulvinar nisi ultrices  eu. Proin vel libero diam. Sed vitae odio tempor, pretium dui ut, faucibus leo. Nunc efficitur justo nunc. Aenean et lacus risus. Duis venenatis orci ut justo euismod, quis rhoncus libero eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porta accumsan hendrerit. Nunc mollis ligula eu metus luctus consequat. Ut      sodales risus sed urna dapibus, dictum pharetra ex bibendum. Aliquam aliquam mi at enim pellentesque tristique. Vivamus pulvinar eu tellus in volutpat.
         </p>
