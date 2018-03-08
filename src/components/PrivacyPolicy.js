b359b4743e9a0c9c0afe69e115718916e259f04e
Jeff Majek <jmaj.email@gmail.com>
2018-03-08 17:49:28 -0500

    Adding terms and privacy pages

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -5,26 +5,37 @@ import { postRefAndPhoneNumber, setRef, setPhoneNumber } from '../actions/index'
 import Typed from 'react-typed';
 import '../copyright-symbol.png';
 import Checkbox from 'material-ui/Checkbox';
-import logo from '../meetable-logo.png';
+import logo from '../ic_meetable_wordmark.svg';
+import meetable_m_logo from '../ic_meetable_icon.svg';
 import wordmark from '../typetreatment_meetable_landingpage.png';
 
 const PrivacyPolicy = () => {
   return (
     <div className='landing-page'>
-     <img src={logo} alt='logo' style={{width: '100px', marginLeft: '-10px', marginTop: '30px'}}/>
-        <h1>
-         <img src={wordmark} alt='Meetable' style={{width: '275px'}}/>
-        </h1>
-    <div>
-        <h2>Privacy Policy</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet sem mauris, at pulvinar nisi ultrices  eu. Proin vel libero diam. Sed vitae odio tempor, pretium dui ut, faucibus leo. Nunc efficitur justo nunc. Aenean et lacus risus. Duis venenatis orci ut justo euismod, quis rhoncus libero eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porta accumsan hendrerit. Nunc mollis ligula eu metus luctus consequat. Ut      sodales risus sed urna dapibus, dictum pharetra ex bibendum. Aliquam aliquam mi at enim pellentesque tristique. Vivamus pulvinar eu tellus in volutpat.
-        </p>
-    </div>
-    <div className="copyright">
-        <p class="smallerText">
-        <img src={require('../copyright-symbol.png')} alt="copyright logo" /> 2018 Meetable  |  more@meetable.com
-        </p>
-     </div>
+      <img src={logo} alt='logo' style={{width: '100px', marginLeft: '-10px', marginTop: '30px'}}/>
+      <h1>
+         <img src={logo} alt='Meetable Logo' />
+      </h1>
+      <div className="tos">
+          <h2>Privacy Policy</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet sem mauris, at pulvinar nisi ultrices  eu. Proin vel libero diam. Sed vitae odio tempor, pretium dui ut, faucibus leo. Nunc efficitur justo nunc. Aenean et lacus risus. Duis venenatis orci ut justo euismod, quis rhoncus libero eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porta accumsan hendrerit. Nunc mollis ligula eu metus luctus consequat. Ut      sodales risus sed urna dapibus, dictum pharetra ex bibendum. Aliquam aliquam mi at enim pellentesque tristique. Vivamus pulvinar eu tellus in volutpat.
+          </p>
+      </div>
+
+
+      <div className="copyright">
+        <div className="left">
+          <img src={meetable_m_logo} alt="M logo" />
+        </div>
+
+        <div className="right">
+          <p>
+            2018 Meetable  |  more@meetable.com
+          </p>
+        </div>
+
+        <br className="clear-both" />
+      </div>
     </div>
   )
 }
--- src/components/TermsOfService.js
+++ src/components/TermsOfService.js
@@ -5,7 +5,8 @@ import { postRefAndPhoneNumber, setRef, setPhoneNumber } from '../actions/index'
 import Typed from 'react-typed';
 import '../copyright-symbol.png';
 import Checkbox from 'material-ui/Checkbox';
-import logo from '../meetable-logo.png';
+import logo from '../ic_meetable_wordmark.svg';
+import meetable_m_logo from '../ic_meetable_icon.svg';
 import wordmark from '../typetreatment_meetable_landingpage.png';
 
 const TermsOfService = () => {
@@ -13,18 +14,29 @@ const TermsOfService = () => {
     <div className='landing-page'>
      <img src={logo} alt='logo' style={{width: '100px', marginLeft: '-10px', marginTop: '30px'}}/>
         <h1>
-         <img src={wordmark} alt='Meetable' style={{width: '275px'}}/>
+         <img src={logo} alt='Meetable Logo' />
         </h1>
-      <div>
+      <div className="tos">
         <h2>Terms of Service</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet sem mauris, at pulvinar nisi ultrices  eu. Proin vel libero diam. Sed vitae odio tempor, pretium dui ut, faucibus leo. Nunc efficitur justo nunc. Aenean et lacus risus. Duis venenatis orci ut justo euismod, quis rhoncus libero eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam porta accumsan hendrerit. Nunc mollis ligula eu metus luctus consequat. Ut      sodales risus sed urna dapibus, dictum pharetra ex bibendum. Aliquam aliquam mi at enim pellentesque tristique. Vivamus pulvinar eu tellus in volutpat.
         </p>
       </div>
-    <div className="copyright">
-        <p class="smallerText">
-        <img src={require('../copyright-symbol.png')} alt="copyright logo" /> 2018 Meetable  |  more@meetable.com
-        </p>
-     </div>
+
+
+      <div className="copyright">
+        <div className="left">
+          <img src={meetable_m_logo} alt="M logo" />
+        </div>
+
+        <div className="right">
+          <p>
+            2018 Meetable  |  more@meetable.com
+          </p>
+        </div>
+
+        <br className="clear-both" />
+      </div>
+
     </div>
   )
 }
