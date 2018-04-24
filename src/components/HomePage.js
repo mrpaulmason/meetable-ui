4c1060c743ba5f12c1c5013fe086f31b356d1054
Paul Mason <paul@meetable.ai>
2018-04-24 18:54:29 -0400

    Slack integration test
    changed back

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -41,7 +41,7 @@ class HomePage extends React.Component {
               </div>
              
               <div className="slogan-text alpha-text">
-                  <p>Text without numbers. No app. No signup. Free.</p>
+                  <p>Text without sharing numbers. No app. No signup. Free.</p>
               </div>
           </section>
 
